512aaa28bec5f475bb6ac55e55ed67f6a1cf6435
vagrant <vagrant@localhost.localdomain>
2019-05-11 17:20:30 +0900

    境界値テストを意識してテストを微修正

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -8,14 +8,23 @@ describe Triangle do
     expect(Triangle.guess_shape([1, 2, 'a'])).to eq '引数は正の整数を与えてください'
   end
   context '三角形の成立条件を満たさない場合' do
-    specify '辺A >= 辺B + 辺Cの場合は「三角形じゃないです＞＜」と返すこと' do
+    specify '辺A = 辺B + 辺Cの場合は「三角形じゃないです＞＜」と返すこと' do
       expect(Triangle.guess_shape([4, 2, 2])).to eq '三角形じゃないです＞＜'
     end
-    specify '辺B >= 辺A + 辺Cの場合は「三角形じゃないです＞＜」と返すこと' do
+    specify '辺A > 辺B + 辺Cの場合は「三角形じゃないです＞＜」と返すこと' do
+      expect(Triangle.guess_shape([5, 2, 2])).to eq '三角形じゃないです＞＜'
+    end
+    specify '辺B = 辺A + 辺Cの場合は「三角形じゃないです＞＜」と返すこと' do
       expect(Triangle.guess_shape([2, 4, 2])).to eq '三角形じゃないです＞＜'
     end
-    specify '辺C >= 辺A + 辺Bの場合は「三角形じゃないです＞＜」と返すこと' do
+    specify '辺B > 辺A + 辺Cの場合は「三角形じゃないです＞＜」と返すこと' do
+      expect(Triangle.guess_shape([2, 5, 2])).to eq '三角形じゃないです＞＜'
+    end
+    specify '辺C = 辺A + 辺Bの場合は「三角形じゃないです＞＜」と返すこと' do
       expect(Triangle.guess_shape([2, 2, 4])).to eq '三角形じゃないです＞＜'
+    end
+    specify '辺C > 辺A + 辺Bの場合は「三角形じゃないです＞＜」と返すこと' do
+      expect(Triangle.guess_shape([2, 2, 5])).to eq '三角形じゃないです＞＜'
     end
   end
   specify 'すべての辺の長さが等しい場合は「正三角形ですね！」と返すこと' do
